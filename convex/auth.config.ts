81deed05d8d31ebb104a0b285bf4a647db1e698f
Kunj Hirapara <kunjhirapara2@gmail.com>
2026-05-09 22:06:09 +0530

    docker files added for deployment

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://boss-peacock-93.clerk.accounts.dev",
+      domain: process.env.CLERK_ISSUER_URL ?? "https://boss-peacock-93.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
